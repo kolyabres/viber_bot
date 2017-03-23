fbb8891dea232009a1cd9e613583d7b76d38179e
mykola-breslavskiy <mykola.breslavskiy@binary-studio.com>
2017-03-23 17:16:11 +0200

    clean

--- index.php
+++ index.php
@@ -10,26 +10,17 @@ $botSender = new Sender([
 
 $bot = new Bot(['token' => getenv('API_KEY')]);
 
-$bot
-    ->onConversation(function ($event) use ($bot, $botSender) {
-        return (new \Viber\Api\Message\Text())
-            ->setSender($botSender)
-            ->setText("Enter your tracking number");
-    })
-    ->onText('/.*/', function ($event) use ($bot, $botSender) {
-        $message = $event->getMessage()->getText();
-        $bot->getClient()->sendMessage(
-            (new \Viber\Api\Message\Text())
-                ->setSender($botSender)
-                ->setReceiver($event->getSender()->getId())
-                ->setText("Follow this link https://www.17track.net/en/externalcall?resultDetailsH=156&nums={$message}&fc=0 to see you order status")
-        );
-    })
-    ->run();
+$bot->onConversation(function ($event) use ($bot, $botSender) {
+    return (new \Viber\Api\Message\Text())
+        ->setSender($botSender)
+        ->setText("Enter your tracking number");
+})->onText('/.*/', function ($event) use ($bot, $botSender) {
+    $number = $event->getMessage()->getText();
+    $message = (new \Viber\Api\Message\Text())
+        ->setSender($botSender)
+        ->setReceiver($event->getSender()->getId())
+        ->setText("Follow this link https://www.17track.net/en/externalcall?resultDetailsH=156&nums={$number}&fc=0 to see you order status");
 
+    $bot->getClient()->sendMessage($message);
 
-try {
-
-} catch (Exception $e) {
-
-}
+})->run();
